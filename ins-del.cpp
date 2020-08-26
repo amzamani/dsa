2e1f11ffe10030285b1b44472ed020a419866acc
amzamani <abuholyangels@gmail.com>
2020-08-26 15:55:34 +0530

    ins-del done
    Signed-off-by: amzamani <abuholyangels@gmail.com>

--- ins-del.cpp
+++ ins-del.cpp
@@ -35,25 +35,31 @@ int main(){
         /* code */
     }
 
-    cout<<"input the number to delet from the array"<<endl;
+    cout<<"input the number to delete from the array"<<endl;
     int m;
     cin>>m;
     int t =1;
     for (int i = 0; i < length; i++)
     {
         if (a[i]==m){
-        
+                length = length -1;
                 /* code */
-                            a[i] = a[i+1];
+                t=0;
                             
-                            cout<<a[i];
+                            
+                            
+
 
             
             
             /* code */
         }
+            if(t==0){
+                a[i] = a[i+1];
+            }
             
            cout<<a[i];
+           
 
         cout<<endl;
         /* code */
@@ -61,8 +67,75 @@ int main(){
     }
     
 
+    cout<<"input the number to insert at starting of array"<<endl;
+    int o;
+    cin>>o;
+     for (int i = length-1; i >= 0; i--)
+    {
+        /* code */
+        a[i+1] = a[i];
+
+    }
+    a[0] = o;
     
-    
+    for (int i = 0; i < length+1; i++)
+    {   cout<<a[i];
+        cout<<endl;
+        /* code */
+        /* code */
+    }
+    cout<<"input the number to insert at ending of  array"<<endl;
+    int p;
+    cin>>p;
+      
+    length = length +1;
+
+    a[length] = p;
+    for (int i = 0; i < length+1; i++)
+    {   cout<<a[i];
+        cout<<endl;
+        /* code */
+        /* code */
+    }
+    cout<<"input 1 to delete from starting of array"<<endl;
+    int q;
+    cin>>q;
+    if(q==1){
+        int z =1;
+        
+    for (int i = 0; i < length; i++)
+    {
+        z=0;
+            if(z==0){
+                a[i] = a[i+1];
+            }
+            
+           cout<<a[i];
+           
+
+        cout<<endl;
+        /* code */
+        /* code */
+    }
+
+    }
+
+    cout<<"input 2 to delete from ending of array"<<endl;
+    int s;
+    cin>>s;
+    if(s==2){
+        length--;
+        for (int i = 0; i < length; i++)
+        {
+            cout<<a[i];
+            cout<<endl;
+            
+            /* code */
+        }
+        
+
+
+    }
     
     return 0;
 
